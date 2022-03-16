3859dda5f64992e8d0e6d5922fb661bd1acc2111
Ardo Sianturi <ardo1978@gmail.com>
2022-03-16 07:58:16 +0700

    upload projek react redux movies app

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>Created By:</div>
+      <div>©ardocoder 2022</div>
+    </div>
+  );
+};
+
+export default Footer;
